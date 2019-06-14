5ea1c88eeefb94d13373ca65b66f06adb66f76c2
Wakatabi <t_wakatabi@uluru.jp>
2019-06-14 19:44:54 +0900

    bean構成ファイルでAspect、DIする

--- AOPApp1/src/main/java/zoku/spring/sample/aop/App.java
+++ AOPApp1/src/main/java/zoku/spring/sample/aop/App.java
@@ -1,17 +1,17 @@
 package zoku.spring.sample.aop;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 	private static ApplicationContext app;
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		app = new ClassPathXmlApplicationContext("bean.xml");
+		app = new AnnotationConfigApplicationContext(MyBeanConfig.class);
 		IMyBean<String> bean = (IMyBean<String>) app.getBean("bean1");
 		bean.addData("Hello AOP World");
-		bean.addData("this is sample data.");
+		bean.addData("this is sample with Config Class.");
 
 		System.out.println(bean);
 	}
---
+++ AOPApp1/src/main/java/zoku/spring/sample/aop/MyBeanConfig.java
@@ -0,0 +1,21 @@
+package zoku.spring.sample.aop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class MyBeanConfig {
+
+	@Bean
+	public MyBean1 bean1() {
+		return new MyBean1();
+	}
+
+	@Bean
+	public MyBeanAspect aop1() {
+		return new MyBeanAspect();
+	}
+
+}
