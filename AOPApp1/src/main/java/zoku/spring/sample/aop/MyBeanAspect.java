f4f61e8d4bfe790484f3a31f58661885a151f976
Wakatabi <t_wakatabi@uluru.jp>
2019-06-14 19:18:29 +0900

    AOPをアノテーションで書き直してみる

--- AOPApp1/src/main/java/zoku/spring/sample/aop/MyBeanAspect.java
+++ AOPApp1/src/main/java/zoku/spring/sample/aop/MyBeanAspect.java
@@ -1,8 +1,14 @@
 package zoku.spring.sample.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
+
+@Aspect
 public class MyBeanAspect {
+	
+	@Before("execution(* zoku.spring.sample.aop.IMyBean.addData(..))")
 	public void addDataBefore(JoinPoint joinpoint) {
 		System.out.println("*addData before...*");
 		String args = "args: \"";
