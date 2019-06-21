f526a9aaeab373eb78b8840b11102d2015046c81
Wakatabi <t_wakatabi@uluru.jp>
2019-06-21 18:46:33 +0900

    MyBeanKeeperを構成ファイルに登録してみる

--- MySampleApp2/src/main/java/zoku/sample/spring/sample2/App.java
+++ MySampleApp2/src/main/java/zoku/sample/spring/sample2/App.java
@@ -1,9 +1,5 @@
 package zoku.sample.spring.sample2;
 
-import java.beans.PropertyDescriptor;
-
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,17 +9,9 @@ public class App {
 	public static void main(String[] args) {
 		app = new ClassPathXmlApplicationContext("classpath:/bean.xml");
 		MyBean bean = (MyBean) app.getBean("bean1");
+		System.out.println(bean);
 
-		bean.setMessage("Hello, How are you ?");
-		MyBeanKeeper keeper = new MyBeanKeeper(bean, "taro@yamada", "hanako@flower");
-		BeanWrapper wrapper = new BeanWrapperImpl(keeper);
-
-		PropertyDescriptor[] descriptors = wrapper.getPropertyDescriptors();
-		for (PropertyDescriptor descriptor : descriptors) {
-			String name = descriptor.getName();
-			Object value = wrapper.getPropertyValue(name);
-			System.out.println(name + " : " + value);
-		}
-
+		MyBeanKeeper beanKeeper = (MyBeanKeeper) app.getBean("beankeeper1");
+		System.out.println(beanKeeper);
 	}
 }
