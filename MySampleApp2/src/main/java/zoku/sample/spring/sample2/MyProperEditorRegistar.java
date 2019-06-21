df8f4b1e05d0e086aa4a601d5cee0ce4f55fa0f0
Wakatabi <t_wakatabi@uluru.jp>
2019-06-21 19:40:02 +0900

    プロパティエディタを作成してみる

---
+++ MySampleApp2/src/main/java/zoku/sample/spring/sample2/MyProperEditorRegistar.java
@@ -0,0 +1,20 @@
+package zoku.sample.spring.sample2;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+
+public class MyProperEditorRegistar implements PropertyEditorRegistrar {
+
+	@Override
+	public void registerCustomEditors(PropertyEditorRegistry registry) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		CustomDateEditor editor = new CustomDateEditor(format, false);
+		registry.registerCustomEditor(Date.class, editor);
+	}
+
+}
